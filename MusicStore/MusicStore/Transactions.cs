a5c03e608b837888fab689035ee70b7b320ca629
RadekOlenski <radek.olenski@gmail.com>
2016-03-18 15:40:28 +0100

    #3 Structure of Changing Data Class

---
+++ MusicStore/MusicStore/Transactions.cs
@@ -0,0 +1,14 @@
+﻿#region includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace MusicStore
+{
+    class Transactions
+    {
+    }
+}
