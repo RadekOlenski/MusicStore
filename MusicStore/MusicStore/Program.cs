a591d7bfea79b62ab6e9cb27c91b941f82a52c09
RadekOlenski <radek.olenski@gmail.com>
2016-03-18 15:29:21 +0100

    Creation of Solution for project

---
+++ MusicStore/MusicStore/Program.cs
@@ -0,0 +1,17 @@
+﻿#region includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace MusicStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
