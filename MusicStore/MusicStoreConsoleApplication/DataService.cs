2c3607512f218ac37b5cf9f30eb33fbd92b2bc94
RadekOlenski <radek.olenski@gmail.com>
2016-04-06 09:56:54 +0200

    Creation of DataService with Dependency Injection of DataRepository

---
+++ MusicStore/MusicStoreConsoleApplication/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.ConsoleApplication
+{
+    class DataService
+    {
+        #region Properties
+        public DataRepository DataRepository { get; set; }
+
+        #endregion
+
+
+    }
+}
