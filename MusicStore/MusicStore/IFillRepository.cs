4944253b6f1c05508c51aac6ffcda33ca2aec5b2
RadekOlenski <radek.olenski@gmail.com>
2016-04-08 17:23:20 +0200

    Poprawki commitów

---
+++ MusicStore/MusicStore/IFillRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore
+{
+    public interface IFillRepository
+    {
+        void CreateClients(List<Client> Clients);
+        void CreateProducts(Dictionary<int, Product> Products);
+        void CreateTransactions(ObservableCollection<Transaction> Transactions);
+    }
+}
---
+++ MusicStore/MusicStoreConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataRepository _DataRepository = new DataRepository();
+            _DataRepository.FillRepository = new FillRepository();
+            _DataRepository.FillClients();
+            _DataRepository.FillProducts();
+            _DataRepository.FillTransactions();
+            Console.WriteLine(_DataRepository.ReadAllClients());
+            Console.WriteLine(_DataRepository.ReadAllProducts());
+            Console.WriteLine(_DataRepository.ReadAllTransactions());
+
+            DataService _DataService = new DataService();
+            _DataService.DataRepository = _DataRepository;
+
+            Console.ReadLine();
+        }
+    }
+}
