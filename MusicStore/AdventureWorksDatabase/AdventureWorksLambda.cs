28a2986aa59c7405436f780e99b0eb72c745938c
RadekOlenski <radek.olenski@gmail.com>
2016-05-15 15:53:24 +0200

    DONE AdventureWorks with Lambdas + Tests

---
+++ MusicStore/AdventureWorksDatabase/AdventureWorksLambda.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorksDatabase
+{
+    public class AdventureWorksLambda
+    {
+        private static AdventureWorksLinqToSqlDataContext db = new AdventureWorksLinqToSqlDataContext();
+
+        #region subsection a)
+        public static List<string> getProductNamesByVendorName(string vendorName)
+        {
+            var products = from product in db.Products
+                           join productVendor in db.ProductVendors
+                           on product.ProductID equals productVendor.ProductID
+                           join vendor in db.Vendors
+                           on productVendor.BusinessEntityID equals vendor.BusinessEntityID
+                           select new { ProductName = product.Name, VendorName = vendor.Name };
+
+            List<string> result = new List<string>();
+            foreach (var item in products.Where(n => n.VendorName.Equals(vendorName)))
+            {
+                result.Add(item.ProductName);
+            }
+            return result;
+        }
+        #endregion
+
+        #region subsection b)
+        public static List<Product> getRecentlyReviewedProducts(int howManyReviews)
+        {
+            List<Product> result = new List<Product>();
+            var query = (from productReview in db.ProductReviews
+                         orderby productReview.ProductReviewID descending
+                         select productReview.ProductID).Take(howManyReviews);
+
+            foreach (var item in db.Products.Where(n => query.Contains(n.ProductID)))
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+        #endregion
+
+        #region subsection c)
+        public static List<Product> getNRecentlyReviewedProducts(int howManyProducts)
+        {
+            List<Product> result = new List<Product>();
+            var query = (from productReview in db.ProductReviews
+                         orderby productReview.ReviewDate descending
+                         select productReview.ProductID).Take(howManyProducts);
+
+            foreach (var item in db.Products.Where(n => query.Contains(n.ProductID)))
+            {
+                result.Add(item);
+            }
+            return result;
+        }
+        #endregion
+
+        #region subsection d)
+        public static List<Product> getNProductsSortedByCategory(int howManyProducts)
+        {
+            var query = (from product in db.Products
+                         join subcategory in db.ProductSubcategories
+                         on product.ProductSubcategoryID equals subcategory.ProductSubcategoryID
+                         join productCategory in db.ProductCategories
+                         on subcategory.ProductCategoryID equals productCategory.ProductCategoryID
+                         orderby product.Name ascending
+                         orderby productCategory.Name ascending
+                         select product).Take(howManyProducts);
+            List<Product> result = new List<Product>();
+            foreach (Product product in query)
+            {
+                result.Add(product);
+            }
+            return result;
+        }
+        #endregion
+
+        #region subsection e)
+        public static decimal getTotalStandardCostByCategory(ProductCategory category)
+        {
+            var query = from productCategory in db.ProductCategories
+                        join subcategory in db.ProductSubcategories
+                        on productCategory.ProductCategoryID equals subcategory.ProductCategoryID
+                        where productCategory.Name == category.Name
+                        select subcategory.ProductSubcategoryID;
+
+            decimal result = 0;
+            foreach (var item in db.Products.Where(n => query.Contains(n.ProductSubcategory.ProductSubcategoryID)))
+            {
+                result += item.StandardCost;
+            }
+            return result;
+        }
+        #endregion
+
+    }
+}
---
+++ MusicStore/LinqQueriesTest/AdventureWorksLambdaTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventureWorksDatabase;
+using System.Linq;
+
+namespace LinqQueriesTest
+{
+    [TestClass]
+    public class AdventureWorksLambdaTest
+    {
+        [TestMethod]
+        public void getProductNamesByVendorNameTest()
+        {
+            List<string> products = AdventureWorksLambda.getProductNamesByVendorName("International");
+            Assert.AreEqual("Lower Head Race", products[0]);
+
+            //Compare with LINQ without Lambda
+            List<string> expected = AdventureWorksLinq.getProductNamesByVendorName("International");
+            CollectionAssert.AreEqual(expected, products);
+        }
+
+        [TestMethod]
+        public void getRecentlyReviewedProductsTest()
+        {
+            List<Product> products = AdventureWorksLambda.getRecentlyReviewedProducts(3);
+            Assert.IsTrue(products.Count == 2);
+        }
+
+        [TestMethod]
+        public void getNRecentlyReviewedProductsTest()
+        {
+            List<Product> products = AdventureWorksLambda.getNRecentlyReviewedProducts(1);
+            Assert.IsTrue(products.Count == 1);
+        }
+
+        [TestMethod]
+        public void getNProductsSortedByCategoryTest()
+        {
+            List<Product> products = AdventureWorksLambda.getNProductsSortedByCategory(10);
+            Assert.IsTrue(products.Count == 10);
+        }
+
+        [TestMethod]
+        public void getTotalStandardCostByCategoryTest()
+        {
+            AdventureWorksLinqToSqlDataContext db = new AdventureWorksLinqToSqlDataContext();
+            var query = (from category in db.ProductCategories
+                         select category).Take(1);
+            decimal actual = AdventureWorksLambda.getTotalStandardCostByCategory(query.First());
+            Assert.AreEqual((decimal)92092.8230, actual);
+             
+            decimal expected = AdventureWorksLinq.getTotalStandardCostByCategory(query.First());
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
